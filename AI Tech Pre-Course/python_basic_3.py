eb562a501698b239e5ccd9b4b376fbd7b5152b49
이다솔 <hodoosol@naver.com>
2021-06-10 00:46:48 +0900

    Delete python_basic_3.py

--- AI Tech Pre-Course/python_basic_3.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-mystr = sorted(input())
-cnt = collections.Counter(mystr)
-M = max(list(cnt.values()))
-
-for i in range(97, 123) :
-    if cnt[chr(i)] == M :
-        print(chr(i), end='')
